40aed991a2f433df6556e9ec4ecced5292a88801
Ahmed Amen <ahmed1amen.g@gmail.com>
2020-12-21 03:13:14 +0200

    Fix Slider Route

--- Modules/Category/Database/Factories/ModelFactory.php
+++ Modules/Category/Database/Factories/ModelFactory.php
@@ -4,9 +4,10 @@ use Modules\Category\Entities\Category;
 
 $factory->define(Category::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->word(),
+      //  'name' => $faker->word(),
         'slug' => $faker->slug(),
-        'on_navigation' => $faker->boolean(),
+        //'on_navigation' => $faker->boolean(),
         'is_active' => $faker->boolean(),
+        'is_searchable' => $faker->boolean(),
     ];
 });
